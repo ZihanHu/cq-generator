89ebf93271edf61eef169e8d6720966c85232c53
Zihan Hu <zihan-hu@foxmail.com>
2023-05-13 13:08:14 +0800

    feat: 上传现有代码

---
+++ app.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+import '@arco-design/web-vue/dist/arco.css';
+
+useHeadSafe({
+  htmlAttrs: {
+    lang: 'zh-CN',
+  },
+  title: '口算训练生成器',
+});
+</script>
+
+<template>
+  <ALayoutContent>
+    <NuxtPage />
+  </ALayoutContent>
+</template>
+
+<style lang="postcss">
+html, body, #__nuxt {
+  @apply w-full h-full;
+}
+#__nuxt {
+  @apply flex flex-col;
+}
+.paper-a4 {
+  width: 210mm;
+  height: 297mm;
+  padding-top: 2.54cm;
+  padding-bottom: 2.54cm;
+  padding-left: 3.18cm;
+  padding-right: 3.18cm;
+}
+</style>
